f043d580e8d31f27561c1c8c781d45de8e9dbe76
virtual <1185513330@qq.com>
2016-03-08 14:38:24 +0800

    角色查询 Signed-off-by: virtual <1185513330@qq.com>

---
+++ GPermission.IQueryServices/IRoleQueryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPermission.IQueryServices
+{
+    /// <summary>角色查询相关接口
+    /// </summary>
+    public interface IRoleQueryService
+    {
+    }
+}
---
+++ GPermission.QueryServices/RoleQueryService.cs
@@ -0,0 +1,16 @@
+﻿using GPermission.IQueryServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPermission.QueryServices
+{
+    /// <summary>角色相关查询
+    /// </summary>
+    public class RoleQueryService : BaseQueryService, IRoleQueryService
+    {
+
+    }
+}

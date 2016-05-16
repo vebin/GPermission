ba5a5fdca791664c346d1d54ec7b004bd1b7e48e
virtual <1185513330@qq.com>
2016-05-16 22:27:46 +0800

    nuget
    Signed-off-by: virtual <1185513330@qq.com>

---
+++ GPermission.Admin/Api/ModulePermissionController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ENode.Commanding;
+
+namespace GPermission.Admin.Api
+{
+    public class ModulePermissionController : BaseApiController
+    {
+
+        public ModulePermissionController(ICommandService commandService)
+            : base(commandService)
+        {
+
+        }
+
+        // GET: api/ModulePermission
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ModulePermission/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ModulePermission
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ModulePermission/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ModulePermission/5
+        public void Delete(int id)
+        {
+        }
+
+      
+    }
+}
